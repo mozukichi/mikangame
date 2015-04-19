2f153d41ca233ab49d4f37ca121a06ed4a8b52a4
zukkun <zukkun@iwaboos-mba13.lan>
2015-04-19 18:40:14 +0900

    操作性を良くした

--- js/input.js
+++ js/input.js
@@ -3,6 +3,12 @@
  */
 var Input = {
 
+    // キーの押下状態
+    state: {
+        37: false,
+        39: false
+    },
+
     // 箱の移動状態
     moveType: null // 左:"left", 右:"right"
 
@@ -16,19 +22,25 @@ Input.init = function() {
 
     // 左右カーソルキーイベント 押下時
     window.addEventListener('keydown', function(e) {
-      switch (e.keyCode) {
-        case 37:
-          Input.moveType = 'left';
-          break;
-        case 39:
-          Input.moveType = 'right';
-          break;
-      }
+        Input.state[e.keyCode] = true;
+
+        switch (e.keyCode) {
+            case 37:
+                Input.moveType = 'left';
+                break;
+            case 39:
+                Input.moveType = 'right';
+                break;
+        }
     });
 
     // キーを離した時イベント
-    window.addEventListener('keyup', function() {
-        Input.moveType = null;
+    window.addEventListener('keyup', function(e) {
+        Input.state[e.keyCode] = false;
+
+        if (Input.state[37] == false && Input.state[39] == false) {
+            Input.moveType = null;
+        }
     });
 
 }
